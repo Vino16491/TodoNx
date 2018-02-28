3928efdb069a46afb6815cf269d91104485d1163
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-03-01 02:04:20 +0530

    New Page generated for adding items in todo List

---
+++ src/pages/add-item/add-item.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-add-item',
+  templateUrl: 'add-item.html',
+})
+export class AddItemPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddItemPage');
+  }
+
+}
