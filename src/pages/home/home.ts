5e04746ecd1504050963009569b0ae240d18cf59
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-03-01 02:49:56 +0530

    Functionality added in home page to pass data on itemDetail page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController } from 'ionic-angular';
-import { AddItemPage } from '../add-item/add-item'
+import { AddItemPage } from '../add-item/add-item';
+import { ItemDetailPage } from '../item-detail/item-detail';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -25,6 +26,8 @@ export class HomePage {
   }
 
   viewItem(item) {
-    alert('This is your item' + JSON.stringify(item))
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
   }
 }
