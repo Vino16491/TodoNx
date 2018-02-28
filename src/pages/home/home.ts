f4db4cb8787077615bf1f5c0060790a336be204d
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-03-01 01:58:59 +0530

    Basic Function added to Home Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items;
   constructor(public navCtrl: NavController) {
 
   }
+  ionViewDidLoad() {
+    this.items = [
+      { title: 'hi', description: 'here You are' },
+      { title: 'hello', description: 'Hey' },
+      { title: 'hey', description: 'jyane' }
+    ]
+  }
 
+  addItem(){
+    alert('Add Item works')
+  }
+
+  viewItem(item){
+    alert('This is your item' + JSON.stringify(item) )
+  }
 }
